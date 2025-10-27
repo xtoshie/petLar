4a084031263dc76f08b8476a2eb38bd7f2279a3f
xtoshie <wtadatochii@gmail.com>
2025-10-26 21:28:29 -0300

    chore(submission): package CSS phase (css/)

--- javascript/footer.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () { // Espera o carregamento completo do DOM
-  const footer = document.createElement('footer');  // Cria o elemento footer
-  footer.innerHTML = '<p>&copy; 2025 PetLar. Todos os direitos reservados.</p>';  // Conteúdo do footer
-  document.body.appendChild(footer);  // Adiciona o footer ao final do body
-});
